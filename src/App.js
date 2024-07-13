869c61c4584c85cd782dd7e5a777024f35bd32a6
karthikmanimaheshuni <117149329+karthikmanimaheshuni@users.noreply.github.com>
2024-07-14 00:42:46 +0530

    frontend(in My-app folder)
    frontend with out node modules

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import QuestionPaperGenerator from './QuestionPaperGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Question Paper Generator</h1>
+        <QuestionPaperGenerator />
+      </header>
+    </div>
+  );
+}
+
+export default App;
